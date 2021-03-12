b7afc9438ad9aa7e5c29def23f4dc369aff12bd0
viranjali <barahate6767@gmail.com>
2021-03-12 14:52:56 +0530

    constraintupdate

--- Chain.js
+++ Chain.js
@@ -1,20 +1,26 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
-
+     fly(){
+     this.chain.bodyA =null;   
+    }
     display(){
+        if(this.chain.bodyA){
+
+       
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.chain.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
     }
+        }
+    }
     
-}
